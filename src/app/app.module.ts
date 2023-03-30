c8f245285724f0dc98bf920b3e1f0e2fb0ed0d88
marcos carvalho <marcosantcarvalho99@gmail.com>
2023-03-30 10:20:40 -0300

    Criação da base HTML e CSS do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConselhosComponent } from './component/conselhos/conselhos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConselhosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/component/conselhos/conselhos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conselhos',
+  templateUrl: './conselhos.component.html',
+  styleUrls: ['./conselhos.component.css']
+})
+export class ConselhosComponent {
+
+}
